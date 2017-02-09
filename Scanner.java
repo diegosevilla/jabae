a14dc6a9f0c6eddb38d4d2197518e567b852df7e
Diego <josevill1@up.edu.ph>
2017-02-09 14:28:33 +0800

    Update Scanner

--- Scanner.java
+++ Scanner.java
@@ -3,15 +3,45 @@ import java.io.FileReader;
 
 public class Scanner{
     public static void main(String args[]){
+        //initialize lexemes
+        
         try{
             //check ba muna kung tamang file type yung ioopen? kunwari check muna kung ".java" siya ganon ganon?
+            if(!args[0].endsWith(".jbe")) {
+                System.out.println("Invalid file type");
+                return;
+            }
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
             String line;
+            int lineNum = 0;
             while((line = br.readLine()) != null){
-                System.out.println(line);
+                lineNum++;
+                if(line.startsWith("//")) continue; //skip comments
+                String token = "";
+                for(int i = 0 ; i < line.length() ; i++){
+                    if(line.charAt(i) == ' '){
+                       //lexemes.print(token, lineNum)
+                       while(line.charAt(i) == ' ')  i++;
+                       lexemes.print(token);
+                       token = "";
+                    } 
+                    if(line.charAt(i) == '"'){
+                       for(i++ ; line.charAt(i) != '"' ; i++){
+                          token+= line.charAt(i);
+                       }
+                    } else 
+                       token += line.charAt(i);
+                    
+                }
+                
+                System.out.println(token);
             }
         }catch(Exception e){
             e.printStackTrace();
         }
     }
 }
+
+//Sho'me12345"Hello World"
+
+
